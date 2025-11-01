84dc4ac9b04f24ec9c903c50183f6b54f4530494
moaz <143252699+bariq49@users.noreply.github.com>
2025-11-01 21:43:56 +0500

    m

--- actions/accept-payment-stripe.ts
+++ actions/accept-payment-stripe.ts
@@ -3,7 +3,7 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2025-06-30.basil", // ✅ CORRECT VERSION
+  apiVersion: "2025-08-27.basil", 
 });
 
 export async function createPaymentIntent({ amount }: { amount: number }) {
